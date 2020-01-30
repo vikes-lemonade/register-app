e5f113bfcf72f0fc58df860702f6cca45d556ea3
ogcolet <ctucker@firstorion.com>
2020-01-30 13:09:42 -0600

    init

---
+++ layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app dark>
+    <v-app-bar>
+      <v-toolbar-title headline font-weight-light>Register App</v-toolbar-title>
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+
+    <v-footer>
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  data: () => ({
+
+  }),
+  // async asyncData({store}) {
+  //   await store.dispatch("SomeRandomAction")
+  // },
+  methods: {
+
+  }
+}
+</script>
