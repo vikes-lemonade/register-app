74ab1c92927a64d0567b06144963f09f1dff7d13
AdamGreenfield <adamjg01@gmail.com>
2020-02-06 10:01:51 -0600

    init

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
     <v-card>
-      <v-card-title>
-        This is a card
-      </v-card-title>
+       <v-card-title>
+         This is a card
+       </v-card-title>
+      <v-card-text>
+         <v-subheader class>Which state?</v-subheader>
+         <v-autocomplete :items="states"></v-autocomplete>
+      </v-card-text>
     </v-card>
   </div>
 </template>
@@ -12,7 +16,22 @@
 
 export default {
   data: () => ({
-
+    states: [
+      'Alabama', 'Alaska', 'American Samoa', 'Arizona',
+      'Arkansas', 'California', 'Colorado', 'Connecticut',
+      'Delaware', 'District of Columbia', 'Federated States of Micronesia',
+      'Florida', 'Georgia', 'Guam', 'Hawaii', 'Idaho',
+      'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky',
+      'Louisiana', 'Maine', 'Marshall Islands', 'Maryland',
+      'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi',
+      'Missouri', 'Montana', 'Nebraska', 'Nevada',
+      'New Hampshire', 'New Jersey', 'New Mexico', 'New York',
+      'North Carolina', 'North Dakota', 'Northern Mariana Islands', 'Ohio',
+      'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico',
+      'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee',
+      'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia',
+      'Washington', 'West Virginia', 'Wisconsin', 'Wyoming',
+    ]
   }),
   // async asyncData({store}) {
   //   await store.dispatch("SomeRandomAction")
