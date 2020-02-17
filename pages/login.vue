05aaa436f74d7018076dcc226ef50f8e8187252e
AdamGreenfield <adamjg01@gmail.com>
2020-02-17 11:25:22 -0600

    Login page template

---
+++ pages/login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <form>
+        <v-text-field label="Employee ID" v-model="id" required></v-text-field>
+        <v-text-field label="Password" v-model="password" required></v-text-field>
+        <v-btn @click="login">Submit</v-btn>
+      </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      id: "",
+      password: ""
+    }),
+    methods: {
+      login(){
+        this.$router.replace({ name:"index" })
+      }
+    }
+  }
+</script>
