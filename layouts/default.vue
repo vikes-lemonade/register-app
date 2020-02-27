b927cb566a9dbebe23d7c81ee93dcca15a510a64
Tay Pham <xuantaypham2@gmail.com>
2020-02-27 15:55:17 -0600

    Feature/main menu (#3)
    * Starter code for main menu, added buttons to toolbar
    
    * using new api route
    
    * Just for the requirements of sprint 2

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,35 @@
 <template>
   <v-app dark style="background-color: #BDBDBD">
-    <v-app-bar dense app>
+    <v-toolbar>
       <v-toolbar-title headline font-weight-light>Register App</v-toolbar-title>
-    </v-app-bar>
+      <v-spacer/>
+      <v-toolbar-items>
+        <!--  <v-btn v-for="(item, index) in items" :key="index" :to="item.to" text>
+          <v-icon>{{item.icon}}</v-icon>{{item.title}}
+        </v-btn>
+        -->
+        <v-btn :to="items[0].to" >
+          <v-icon>{{items[0].icon}}</v-icon>{{items[0].title}}
+        </v-btn>
+        <v-btn :to="items[1].to" >
+          <v-icon>{{items[1].icon}}</v-icon>{{items[1].title}}
+        </v-btn>
+        <v-btn :to="items[2].to" v-on:click="say('Function has not been implemented!')">
+          <v-icon>{{items[2].icon}}</v-icon>{{items[2].title}}
+        </v-btn>
+        <v-btn :to="items[3].to" v-on:click="say('Function has not been implemented!')">
+          <v-icon>{{items[3].icon}}</v-icon>{{items[3].title}}
+        </v-btn>
+        <v-btn :to="items[4].to" v-on:click="say('Function has not been implemented!')">
+          <v-icon>{{items[4].icon}}</v-icon>{{items[4].title}}
+        </v-btn>
+        <v-btn :to="items[5].to" v-on:click="say('Function has not been implemented!')">
+          <v-icon>{{items[5].icon}}</v-icon>{{items[5].title}}
+        </v-btn>
+
+      </v-toolbar-items>
+
+    </v-toolbar>
     <v-content>
       <v-container fluid>
         <nuxt />
@@ -16,19 +43,45 @@
 </template>
 
 <script>
+
 export default {
+  methods: {
+    say: function (msg) {
+      alert(msg)
+    }
+  },
   data () {
     return {
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
+          icon: 'home',
+          title: 'Home',
           to: '/'
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          icon: '',
+          title: 'Product Listing',
+          to: '/'
+        },
+        {
+          icon: '',
+          title: 'Start Transaction',
+          to: '/'
+        },
+        {
+          icon: '',
+          title: 'Employee Detail',
+          to: '/'
+        },
+        {
+          icon: '',
+          title: 'Cashier Report',
+          to: '/'
+        },
+        {
+          icon: '',
+          title: 'Sign out',
+          to: '/'
         }
       ],
     }
